3c9f64745efae05180044128c5ff9d336886c500
NITHI2209 <nithyasree065@gmail.com>
2025-06-19 10:50:33 +0530

    Create bistuies operator.c

---
+++ bistuies operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+   int age=16;
+//    age>=18?printf("you are eligible to vote"):printf("Your are not eligible to vote");
+   int a=age>=18?1:0;
+   printf("%d:",a);
+   return 0;
+}
