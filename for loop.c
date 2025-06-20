b8a8dc18a5df5e51fac6acbf4bc3622ed992ed6e
NITHI2209 <nithyasree065@gmail.com>
2025-06-20 10:26:49 +0530

    Create for loop.c

---
+++ for loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   int i;
+//    for(i=1;i<=100;i=i+1)
+//    {
+//        printf("%d:",i);
+//    }
+//   for(i=1;i<=100;i=i+2)
+//    {
+//        printf("%d:",i);
+//    }
+   for(i=1;i<=100;i=i+3)
+   {
+       printf("%d:",i);
+   }
+    return 0;
+}
