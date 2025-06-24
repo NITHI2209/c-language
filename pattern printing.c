5d4dbafb7b90ab427d9e409e60642c94f17dcfc2
NITHI2209 <nithyasree065@gmail.com>
2025-06-24 10:18:27 +0530

    Create pattern printing.c

---
+++ pattern printing.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int row,col;
+    int i,j;
+    printf("Enter the row and col\n");
+    scanf("%d %d",&row,&col);
+    for(i=1;i<=row;i++)
+    {
+       for(j=1;j<=col;j++)
+       {
+           i%2==0?printf("0"):printf("1");
+       }
+       printf("\n");
+    }
+    return 0;
+}
