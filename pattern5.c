49f7c3565fe7229ea8ad48ae8213a9aef23b5e27
NITHI2209 <nithyasree065@gmail.com>
2025-06-24 11:49:18 +0530

    Create pattern5.c

---
+++ pattern5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i,j;
+    printf("Enter your n value\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+       for(j=1;j<=n;j++)
+       {
+           if(i==1||i==n||j==1||j==n)
+           {
+              printf("1");
+           }
+           else
+           {
+              printf(" ");
+           }
+       }
+       printf("\n");
+    }
+    return 0;
+}
