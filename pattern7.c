161ca166f0427b2b6d97b1df0cc259a317c8679a
NITHI2209 <nithyasree065@gmail.com>
2025-06-24 12:18:34 +0530

    Create pattern7.c

---
+++ pattern7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i,j;
+    printf("Enter your n value\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+       for(j=1;j<=n;j++)
+       {
+           if(i==1||i==n||j==1||j==n||i==j||i+j==n+1)
+           {
+              printf("1");
+           }
+           else
+           {
+              printf(" ");
+           }
+       }
+       printf("\n");
+    }
+    return 0;
+}
